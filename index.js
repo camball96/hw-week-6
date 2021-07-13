da735c07f621a23608e8575bfa9ddc2b35531582
Cam <cam.ball96@hotmail.com>
2021-07-13 23:11:48 +1000

    some early work on js with linking apis  and more work on HTML

---
+++ index.js
@@ -0,0 +1,48 @@
+const btn = document.querySelector("#citySearchBtn");
+const citySearched = document.querySelector("#yourCity").value; 
+const currTemp = document.querySelector("#currTemp");
+const currHum = document.querySelector("#currHum");
+const currWind = document.querySelector("#currWind");
+const fiveDayCard = document.querySelector(".fiveDayCard");
+
+//const apiForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearched}&Appid=2ffd9582b4b57a4f7c33fcb8f4e2e5c6&units=metric`
+//`https://api.openweathermap.org/data/2.5/weather?q=melbourne&Appid=2ffd9582b4b57a4f7c33fcb8f4e2e5c6&units=imperial"`
+
+
+//updates dom on results container
+btn.addEventListener("click", () => {
+
+    const citySearched = document.querySelector("#yourCity").value;
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${citySearched},au&units=metric&appid=2ffd9582b4b57a4f7c33fcb8f4e2e5c6`
+    const apiForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearched},au&Appid=2ffd9582b4b57a4f7c33fcb8f4e2e5c6&units=metric`
+
+    $('#currDate').text(moment().format('MMMM Do YYYY, h:mm a'));
+
+    console.log(citySearched);
+    
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const {temp, humidity,} = data.main;
+            currTemp.innerHTML = "Temp: " + temp + " degrees c";
+            currHum.innerHTML = "Humidity: " + humidity + "%";
+            const {speed} = data.wind;
+            currWind.innerHTML = "Wind: " + speed + " knots";
+            const cityName = document.querySelector("#city");
+            cityName.innerHTML = citySearched;
+        });
+
+    fetch(apiForecast)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            
+        });    
+    
+
+});
